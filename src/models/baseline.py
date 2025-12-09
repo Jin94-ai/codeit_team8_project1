eb61ff8f97115c1cb6ea8846ee7eda7fc543d49a
bykim0119 <bukim0119@gmail.com>
2025-12-09 19:17:27 +0900

    script README Updated

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -49,4 +49,35 @@ model.train(
     imgsz=640,
 )
 
-model.predict(source="data/test_images/", imgsz=640, save=True)
+results = model.predict(source="data/test_images/", imgsz=640)
+
+
+
+
+rows = []
+annotation_id = 1
+
+for res in results:
+    img_name = os.path.basename(res.path)  # 예: "1.png"
+    image_id = int(os.path.splitext(img_name)[0])  # 파일명 숫자 추출
+    boxes = res.boxes  # Boxes 객체
+    for box, cls, score in zip(boxes.xyxy, boxes.cls, boxes.conf):
+        x1, y1, x2, y2 = box.tolist()
+        bbox_x = int(x1)
+        bbox_y = int(y1)
+        bbox_w = int(x2 - x1)
+        bbox_h = int(y2 - y1)
+        rows.append({
+            "annotation_id": annotation_id,
+            "image_id": image_id,
+            "category_id": int(cls)+1,  # YOLO 클래스 0-based → 캐글 1-based
+            "bbox_x": bbox_x,
+            "bbox_y": bbox_y,
+            "bbox_w": bbox_w,
+            "bbox_h": bbox_h,
+            "score": float(score)
+        })
+        annotation_id += 1
+
+df = pd.DataFrame(rows)
+df.to_csv("submission.csv", index=False)
