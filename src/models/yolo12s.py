65c0e9066a3875960699fa024808dbe2a908bd1d
bykim0119 <bukim0119@gmail.com>
2025-12-15 09:45:34 +0900

    Seed Fix Added

--- src/models/yolo12s.py
+++ src/models/yolo12s.py
@@ -10,7 +10,7 @@ from PIL import Image
 from tqdm.notebook import tqdm
 import random
 from src.models.callbacks import wandb_train_logging, wandb_val_logging
-
+import torch
 import wandb
 from ultralytics import YOLO
 
@@ -41,6 +41,25 @@ if selected_font:
 else:
     print("경고: 사용 가능한 기본 한글 폰트를 찾지 못했습니다. 수동 설정 필요")
 
+
+
+# =================== Seed Fix ===================
+
+
+def seed_fix(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+seed_fix(42)
+# ==============================================
 
 
 ################### Model Run ###################
